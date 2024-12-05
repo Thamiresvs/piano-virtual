acb9b306359b6d837ed63f50084a15821a3ee8cd
Thamires Vasconcelos <thamiresvasconcelos23@gmail.com>
2024-12-05 20:31:56 -0300

    Ajustes para somente as teclas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,12 +1,29 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let mappedKeys = [];
 const audio = new Audio();
 
 const playTune = (key) => {
     audio.src = `src/tunes/${key}.wav`;
     audio.play();
+
+    const clickedKeys = document
+    .querySelector(`[data-key="${key}"]`)
+    clickedKeys.classList.add("active")
+    setTimeout(() => {
+        clickedKeys.classList.remove("active");
+    }, 150);
 };
 
 pianoKeys.forEach((key) => {
     key.addEventListener("click", () =>  playTune(key.dataset.key));
-})
+    mappedKeys.push(key.dataset.key);
+});
+
+document.addEventListener("keydown", 
+    (e) => {
+        if (mappedKeys.includes(e.key)) {
+            playTune(e.key);
+        }
+    }
+);
